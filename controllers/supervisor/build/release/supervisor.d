8fdad5a4ae084246b70254ad89feeb27da057ff7
vijashu <victorhu3@gmail.com>
2020-11-22 12:39:10 -0500

    gitignore stuff

--- controllers/supervisor/build/release/supervisor.d
+++
@@ -1,6 +0,0 @@
-build/release/supervisor.o: supervisor.cpp \
- /usr/local/webots/include/controller/cpp/webots/Supervisor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Node.hpp \
- /usr/local/webots/include/controller/cpp/webots/Field.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp
